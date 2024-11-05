4605adadf68cdc1d45ee3e2d4a552d3babc0a2bd
Mark A. <mark001616@gmail.com>
2024-11-04 20:21:10 -0500

    Add support for reading form data

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 const loginRouter = require("./routes/loginRouter");
 
 app.use("/login", loginRouter);
