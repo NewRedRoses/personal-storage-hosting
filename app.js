dd82c1c45b1c9180f54078304a34803445e6a6d9
Mark A. <mark001616@gmail.com>
2024-11-04 21:39:44 -0500

    Initialize login authentication

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
+
 const app = express();
 
 const PORT = 3000;
@@ -7,7 +12,10 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
 
 const loginRouter = require("./routes/loginRouter");
 
--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,10 +1,60 @@
 const { PrismaClient } = require(`@prisma/client`);
-const prisma = new PrismaClient();
+const passport = require("passport");
 
-const loginRouterGet = async (req, res) => {
+const prisma = new PrismaClient();
+const LocalStrategy = require("passport-local").Strategy;
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: { username: username },
+      });
+      console.log(user, username);
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      // compared hashed psswrd to user passwrd at login
+      //   const match = await bcrypt.compare(password, user.password);
+      if (password != user.password) {
+        console.log("Unable to login: passwords do not match");
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    // Get user ID from db that matches arg id set it ‘user’
+    const user = await prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+const loginRouterGet = async (req, res, next) => {
   res.render("loginPage");
 };
 
-const loginRouterPost = async (req, res) => {};
+const loginRouterPost = async (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })(req, res, next);
+};
 
 module.exports = { loginRouterGet, loginRouterPost };
