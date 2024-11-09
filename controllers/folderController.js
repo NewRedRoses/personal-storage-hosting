1c48f73feea9475d4658e4879cd958202333f73b
Mark A. <mark001616@gmail.com>
2024-11-09 10:55:45 -0500

    Add ability to create and render user folders

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,3 +1,39 @@
-const createFolderPost = async (req, res, next) => {};
+const { PrismaClient } = require(`@prisma/client`);
+const prisma = new PrismaClient();
 
-module.exports = { createFolderPost };
+const createFolderPost = async (req, res, next) => {
+  if (!req.user) {
+    throw new Error("Must be logged in!");
+  }
+  try {
+    await prisma.folder.create({
+      data: {
+        name: "new folder!",
+        user: { connect: { id: req.user.id } },
+      },
+    });
+    res.status(201).send("Folder created successfully!");
+  } catch (error) {
+    console.error("Error creating folder: ", error);
+    next(error);
+  }
+};
+
+const getAllFolders = async (req, res, next) => {
+  if (!req.user || !req.user.id) {
+    return res.status(401).send("User not logged in");
+  }
+  try {
+    const folders = await prisma.folder.findMany({
+      where: {
+        userId: req.user.id,
+      },
+    });
+    return folders;
+  } catch (error) {
+    console.log("Error getting folders: ", error);
+    next(error);
+  }
+};
+
+module.exports = { createFolderPost, getAllFolders };
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,8 @@
-const homeRouterGet = (req, res, next) => {
-  res.render("homePage");
+const { getAllFolders } = require("../controllers/folderController");
+
+const homeRouterGet = async (req, res, next) => {
+  const folders = await getAllFolders(req, res, next);
+  res.render("homePage", { folders: folders });
 };
 const homeRouterPost = (req, res, next) => {
   console.log(req.file);
