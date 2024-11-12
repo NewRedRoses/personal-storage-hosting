7398955cb1a87d7eea47cc0dd3f8b89a9e38b96c
Mark A. <mark001616@gmail.com>
2024-11-11 19:25:16 -0500

    Add CSS support

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 app.use(express.urlencoded({ extended: false }));
 
 app.use(
